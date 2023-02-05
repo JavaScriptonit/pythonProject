d4348245391d9f70877d181f59c1d4a407685705
Andrey Shabunov <shabunovaa@yandex.ru>
2023-02-05 23:50:36 +0500

    05.02.23 Python if/else Conditionals

--- userInput.py
+++ userInput.py
@@ -3,9 +3,20 @@ name_of_unit = "hours"
 
 
 def days_to_units(days):
-    return f"{days} days are {int(days) * calculation_to_units} {name_of_unit}"
+    if days > 0:
+        return f"{days} days are {days * calculation_to_units} {name_of_unit}"
+    elif days == 0:
+        return "You entered a 0, please enter a valid positive number!"
+
+
+def validate_and_execute():
+    if user_input.isdigit():
+        user_input_number = int(user_input)
+        calculated_value = days_to_units(user_input_number)
+        print(calculated_value)
+    else:
+        print("Your input is not a valid number. Please enter a valid positive number!")
 
 
 user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
-calculated_value = days_to_units(user_input)
-print(calculated_value)
+validate_and_execute()
